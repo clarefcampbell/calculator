945f8af46ab677bb4a4a6a268dc3bdf004ade378
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-05-11 21:13:43 +1200

    Create files and initial functions and hmtl

---
+++ script.js
@@ -0,0 +1,20 @@
+/*Global Variable Declaration*/
+let num1 = 0;
+let num2 = 0;
+let oper = '';
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2; 
+}
