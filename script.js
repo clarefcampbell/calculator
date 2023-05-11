aca09acb4fbb38a569b3370c11e69f71c2078c45
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-05-12 01:53:07 +1200

    Add all calculator buttons, initial CSS

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let num1 = 0;
 let num2 = 0;
 let oper = '';
+let result = 0;
+
+/*Functions*/
 
 function add (num1, num2) {
     return num1 + num2;
@@ -18,3 +21,25 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2; 
 }
+
+function operate (oper, num1, num2) {
+    switch (oper) {
+        case '+':
+          result = add(num1, num2);
+          break;
+        case '-':
+            result = subtract(num1, num2);
+        case '*':
+          result = multiply(num1, num2);
+          break;
+        case '/':
+            result = divide(num1, num2);
+        default:
+          console.log('error');
+      }
+}
+
+/*Document query selectors*/
+const historyField = document.querySelector('#history');
+const readoutField = document.querySelector('#readout');
+readoutField.innerHTML = '1';
