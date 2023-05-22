258454649f8ec13569261cb3e5a971970a9de7d4
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-05-22 21:03:42 +1200

    Add initial functionality of entering and displaying numbers

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let num1 = 0;
 let num2 = 0;
 let oper = '';
 let result = 0;
+let displayValue = ''
 
 /*Functions*/
 
@@ -42,4 +43,39 @@ function operate (oper, num1, num2) {
 /*Document query selectors*/
 const historyField = document.querySelector('#history');
 const readoutField = document.querySelector('#readout');
-readoutField.innerHTML = '1';
+const clearBtn = document.querySelector('#clear')
+const equalBtn = document.querySelector('#equals');
+const nums = Array.from(document.querySelector('#numbers').children);
+const opers = Array.from(document.querySelector('#operators').children);
+
+/*Events*/
+nums.forEach(number => number.addEventListener('click', (e) => {
+    if(readoutField.innerHTML != 0){
+        readoutField.innerHTML += e.target.innerHTML;
+    } else {
+        readoutField.innerHTML = e.target.innerHTML;
+    }
+}));
+
+opers.forEach(oper => oper.addEventListener('click', (e) => {
+    if(num1 = 0) {
+        num1 = +readoutField.innerHTML;
+        oper = e.target.innerHTML;
+        historyField.innerHTML += num1 + ' ' + oper + ' ';
+        readoutField.innerHTML = '0';
+    } else {
+        num2 = +readoutField.innerHTML;
+    }
+}))
+
+clearBtn.addEventListener('click', () => {
+    readoutField.innerHTML = '0';
+    historyField.innerHTML = ''; 
+    num1 = 0;
+    num2 = 0; 
+    result = 0;
+});
+
+equalBtn.addEventListener('click', () => {
+    
+});
