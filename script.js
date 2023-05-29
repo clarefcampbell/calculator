0749fbd98f77d4e237226cf078194084fa8d6e5b
Clare <110568186+clarefcampbell@users.noreply.github.com>
2023-05-30 02:29:56 +1200

    Calculator functional with new test design

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /*Global Variable Declaration*/
-let num1 = 0;
-let num2 = 0;
+let num1s = '';
+let num2s = '';
 let operator = 'test';
 let result = 0;
-let displayValue = ''
+currentValue = '';
 
 /*Functions*/
 
@@ -39,9 +39,12 @@ function operate (oper, num1, num2) {
 
 }
 
+function updateDisplay (num1, num2, oper) {
+    display.innerHTML = num1 + ' ' + oper + ' ' + num2;
+}
+
 /*Document query selectors*/
-const historyField = document.querySelector('#history');
-const readoutField = document.querySelector('#readout');
+const display = document.querySelector('#readout');
 const clearBtn = document.querySelector('#clear')
 const equalBtn = document.querySelector('#equals');
 const nums = Array.from(document.querySelector('#numbers').children);
@@ -49,40 +52,40 @@ const opers = Array.from(document.querySelector('#operators').children);
 
 /*Events*/
 nums.forEach(number => number.addEventListener('click', (e) => {
-    if(readoutField.innerHTML != 0){
-        readoutField.innerHTML += e.target.innerHTML;
+    if(currentValue == ''){
+        currentValue = e.target.innerHTML;
+        updateDisplay(currentValue, '', '');
     } else {
-        readoutField.innerHTML = e.target.innerHTML;
+        currentValue += e.target.innerHTML;
+        updateDisplay(currentValue,'','');
     }
 }));
 
 opers.forEach(oper => oper.addEventListener('click', (e) => {
-    if(num1 == 0) {
-        operator = e.target.innerHTML;
-        num1 = +readoutField.innerHTML;
+    if(num1s == '') {
+        num1s = currentValue;
     } else {
-        num2 = +readoutField.innerHTML;
-        console.log('num1 - ' + num1 + ', num2 - ' + num2 + ', oper - ' + operator)
-        result = operate(operator, num1, num2);
-        console.log(result);
-        num1 = result;
+        num2s = currentValue;
+        num1s = operate(operator, +num1s, +num2s);
         operator = e.target.innerHTML;
+        num2s = '';
     }
-    historyField.innerHTML += readoutField.innerHTML + ' ' + operator + ' ';
-    readoutField.innerHTML = 0;
+    operator = e.target.innerHTML;
+    currentValue = ''; 
+    updateDisplay(num1s, num2s, operator);
 }))
 
 clearBtn.addEventListener('click', () => {
-    readoutField.innerHTML = '0';
-    historyField.innerHTML = ''; 
-    num1 = 0;
-    num2 = 0; 
+    display.innerHTML = '0';
+    num1s = 0;
+    num2s = 0; 
     result = 0;
+    currentValue = '';
 });
 
 equalBtn.addEventListener('click', () => {
     console.log(operator);
-    num2 = +readoutField.innerHTML;
+    num2 = +display.innerHTML;
     result = operate(operator, num1, num2);
     console.log('num1 - ' + num1 + ', num2 - ' + num2 + ', oper - ' + operator + ', result - ', result);
 });
